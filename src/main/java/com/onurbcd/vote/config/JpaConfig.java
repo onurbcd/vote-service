0f4646dedab31e15a495bb9dbf202e5aae8cfc71
Bruno Domingues <onurbcd@gmail.com>
2022-05-26 17:06:31 -0300

    add jpa validation config

---
+++ src/main/java/com/onurbcd/vote/config/JpaConfig.java
@@ -0,0 +1,16 @@
+package com.onurbcd.vote.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
+}
