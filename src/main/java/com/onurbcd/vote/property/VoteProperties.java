52c0e95a75ecdf0b4fcc92c93661a1e1427facec
Bruno Domingues <onurbcd@gmail.com>
2022-05-26 21:36:46 -0300

    add web config

---
+++ src/main/java/com/onurbcd/vote/property/VoteProperties.java
@@ -0,0 +1,13 @@
+package com.onurbcd.vote.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties("vote")
+@Configuration("voteProperties")
+@Getter
+@Setter
+public class VoteProperties {
+}
